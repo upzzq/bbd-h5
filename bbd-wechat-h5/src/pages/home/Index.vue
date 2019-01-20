4db21f5ea35c5a15905c6d08c126700d24314ddc
zhouziqi_global <zhouziqi@gmail.com>
2019-01-21 00:37:22 +0800

    初始导入

---
+++ bbd-wechat-h5/src/pages/home/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>微信授权</div>
+    <router-link to="https://www.baidu.com/">百度一下</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style>
+</style>
